ccc08c4b8e9eeaa07c9f0fcf9fd9a5a9f325615e
Akshaan Bandara <53240589+AkshaanB@users.noreply.github.com>
2020-02-29 16:17:02 +0530

    Add files via upload

---
+++ MyWork/db.php
@@ -0,0 +1,12 @@
+<?php
+$dbhost = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "homteq_solutions";
+$conn = mysqli_connect($dbhost,$dbusername,$dbpassword,$dbname);
+if(!$conn){
+    echo "Couldn't connect to database $dbname.";
+}
+mysqli_select_db($conn,$dbname);
+
+?>
